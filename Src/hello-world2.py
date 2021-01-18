0a55c019690c1cfd025b1343e1fe84c020e1e860
usmance <usmankhalid38@outlook.com>
2021-01-19 03:23:49 +0500

    Two Jenkin Files

---
+++ Src/hello-world2.py
@@ -0,0 +1,4 @@
+print("Hello World 2")
+x=[2,4,6,8,10,12,14,16,18,20]
+for i in x:
+    print(i) 
---
+++ Src/test2.py
@@ -0,0 +1,6 @@
+
+def test(num):
+    if num==2:
+        raise ValueError('I am coming from test 2')
+
+test(2)
